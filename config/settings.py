89ebc40e7b206d1bb3463adb3c7c12025c8a694e
Nazifi Balarabe Ibrahim <Capo@Capos.local>
2025-05-29 00:58:37 +0100

    ✅ Remove profit for sales

--- config/settings.py
+++ config/settings.py
@@ -148,13 +148,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 
 
 # Default primary key field type
@@ -172,6 +165,11 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 
 import os
 
+STATIC_URL = '/static/'  # leading slash = essential
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # string path, not Path()
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
